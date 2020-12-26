2096373c9e4fda5c3ee14e2afa3f7e0d02fcac07
rhea-so <jeonghyeon.rhea@gmail.com>
2020-12-26 18:26:56 +0900

    update: rhea-runtime을 을 사용한 웹 서버 서비스 구축

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
 require('app-module-path').addPath(__dirname);
 require('source-map-support').install();
 
+import Runtime from 'rhea-runtime';
+
 import { Debug, LogTag } from 'Utils/debugTool';
 
-Debug.log(LogTag.NOWAY, 'Hello, World!');
+async function main() {
+	Debug.log(LogTag.NOWAY, 'Hello, World!');
+	await Runtime.boot();
+	Runtime.get('/', async (req, res) => {
+		res.send('Hello, World!');
+	});
+	await Runtime.open();
+}
+
+main();
